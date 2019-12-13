2a42fd735570ca245da25964651371ddf2271114
ryoji25 <r25.k0305@gmail.com>
2019-12-13 15:27:32 +0900

    投稿詳細へのリンクを記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,14 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment: comment_params[:comment], post_id: comment_params[:post_id], user_id: current_user.id)
+    comment = Comment.create(comment_params)
+    comment.save
     redirect_to "/posts/#{comment.post.id}"   #コメントと結びつくツイートの詳細画面に遷移する
   end
 
   private
   def comment_params
-    params.permit(:comment, :post_id)
+    params.permit(:comment, :post_id).merge(user_id: current_user.id)
   end
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates_uniqueness_of :post_id, scope: :user_id
 end
