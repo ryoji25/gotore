8f22b4940aa3c80be4f57568bb781755595da9d0
ryoji25 <r25.k0305@gmail.com>
2019-12-19 12:32:19 +0900

    プロフィール設定画面コントローラー・ビュー設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
   def show
     @posts = Post.where(user_id: current_user.id)
   end
+
+  def edit
+    @user = User.find(current_user.id)
+  end
   
 end
