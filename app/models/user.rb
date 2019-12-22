c1cefe4e0eca065e8eb42a206ba8ff08cfe8fb84
ryoji25 <r25.k0305@gmail.com>
2019-12-22 13:46:21 +0900

    userモデルにImageUploaderの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  mount_uploader :image, ImageUploader
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- db/schema.rb
+++ db/schema.rb
@@ -43,16 +43,16 @@ ActiveRecord::Schema.define(version: 20191209050327) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                                              null: false
+    t.string   "name",                                                         null: false
     t.text     "profile",                limit: 65535
-    t.string   "image",                                default: "", null: false
-    t.string   "email",                                default: "", null: false
-    t.string   "encrypted_password",                   default: "", null: false
+    t.string   "image",                                default: "my-page.jpg", null: false
+    t.string   "email",                                default: "",            null: false
+    t.string   "encrypted_password",                   default: "",            null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
+    t.datetime "created_at",                                                   null: false
+    t.datetime "updated_at",                                                   null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
